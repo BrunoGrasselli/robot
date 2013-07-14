1ef5f8577ac5fa8d0a6b716d595e25fe7dccbe89
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-07-14 10:01:36 -0300

    starting to implement the controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,20 @@
 class GameController
+  attr_accessor :board, :robot
+
   def initialize(robot, board)
-    
+    self.board = board
+    self.robot = robot
   end
 
-  def interpret_command(command)
-    
+  def play(command)
+    if command.match /^PLACE (\d+),(\d+),(NORTH|EAST|WEST|SOUTH)$/
+      board.place $1.to_i, $2.to_i, $3.to_sym
+    elsif command == "MOVE"
+      board.place(*robot.move(*board.report))
+    elsif command == "LEFT"
+      board.place(*robot.left(*board.report))
+    elsif command == "RIGHT"
+      board.place(*robot.right(*board.report))
+    end
   end
 end
--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -1,8 +1,55 @@
 require 'spec_helper'
 
 describe GameController do
-  describe "#interpret_command" do
-    pending
+  describe "#play" do
+    let(:board)      { Board.new }
+    let(:robot)      { Robot.new }
+    let(:controller) { described_class.new(robot, board) }
+
+    context "robot is placed" do
+      before do
+        board.place(0,0,:NORTH)
+      end
+
+      context "when command is PLACE" do
+        it "places the robot on the new place if place is valid" do
+          controller.play("PLACE 3,1,NORTH")
+          board.report.should eq [3,1,:NORTH]
+        end
+
+        it "keeps the robot on the previous place if place is invalid" do
+          controller.play("PLACE 10,1,NORTH")
+          board.report.should eq [0,0,:NORTH]
+        end
+      end
+
+      context "when command is MOVE" do
+        it "places the robot on the new place if movement is valid" do
+          controller.play("MOVE")
+          board.report.should eq [0,1,:NORTH]
+        end
+
+        it "keeps the robot on the previous place if movement is invalid" do
+          board.place(0,4,:NORTH)
+          controller.play("MOVE")
+          board.report.should eq [0,4,:NORTH]
+        end
+      end
+
+      context "when command is LEFT" do
+        it "turns the robot" do
+          controller.play("LEFT")
+          board.report.should eq [0,0,:WEST]
+        end
+      end
+
+      context "when command is RIGHT" do
+        it "turns the robot" do
+          controller.play("RIGHT")
+          board.report.should eq [0,0,:EAST]
+        end
+      end
+    end
   end
 end
 

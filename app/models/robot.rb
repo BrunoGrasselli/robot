42031715096ed5228b06ee46ce49448365cea726
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-07-14 00:20:53 -0300

    implementing Robot#move

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,2 +1,16 @@
 class Robot
+  def move(pos_x, pos_y, facing)
+    case facing
+    when :NORTH
+      pos_y += 1
+    when :SOUTH
+      pos_y -= 1
+    when :WEST
+      pos_x -= 1
+    when :EAST
+      pos_x += 1
+    end
+
+    [pos_x, pos_y, facing]
+  end
 end
--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe Robot do
-  
+  let(:robot) { described_class.new }
+  describe "#move" do
+    it "moves to north when robot is facing north" do
+      robot.move(1,1,:NORTH).should eq [1,2,:NORTH]
+    end
+
+    it "moves to south when robot is facing south" do
+      robot.move(2,3,:SOUTH).should eq [2,2,:SOUTH]
+    end
+
+    it "moves to west when robot is facing west" do
+      robot.move(2,3,:WEST).should eq [1,3,:WEST]
+    end
+    
+    it "moves to east when robot is facing east" do
+      robot.move(2,3,:EAST).should eq [3,3,:EAST]
+    end
+  end
 end
 

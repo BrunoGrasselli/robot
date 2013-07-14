7f972b711688b41ec5c4486bf012e5c25ed28b37
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-07-14 00:44:56 -0300

    implementing Board#place

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,25 @@
 class Board
+  LIMIT = 4
+
+  def place(pos_x, pos_y, facing)
+    return false unless valid_position?(pos_x, pos_y)
+
+    self.current_position = pos_x, pos_y, facing
+
+    true
+  end
+
+  def report
+    @current_position
+  end
+
+  private
+
+  def valid_position?(pos_x, pos_y)
+    (0..LIMIT).include?(pos_x) && (0..LIMIT).include?(pos_y)
+  end
+
+  def current_position=(current_position)
+    @current_position = current_position
+  end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,6 +1,34 @@
 require 'spec_helper'
 
 describe Board do
-  
+  let(:board) { described_class.new }
+
+  describe "#place" do
+    before do
+      board.place 0,0,:NORTH
+    end
+
+    context "when position is valid" do
+      it "returns true" do
+        board.place(1,3,:SOUTH).should eq true
+      end
+      
+      it "updates the robot position" do
+        board.place(4,4,:EAST)
+        board.report.should eq [4,4,:EAST]
+      end
+    end
+
+    context "when position is invalid" do
+      it "returns false" do
+        board.place(-1,3,:SOUTH).should eq false
+      end
+
+      it "doesn't update the robot position" do
+        board.place(4,5,:EAST)
+        board.report.should eq [0,0,:NORTH]
+      end
+    end
+  end
 end
 
